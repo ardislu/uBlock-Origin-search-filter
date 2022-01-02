a2dd209af981cb6b06c835d87e2e8454efc08e70
Ardis Lu <ardisglu@gmail.com>
2022-01-02 02:22:54 -0800

    Update build regex to exclude empty lines

--- make-search-filter.sh
+++ make-search-filter.sh
@@ -7,11 +7,11 @@ echo '! Expires: 14 days' >>'search-filter.txt'
 echo '! Homepage: https://github.com/ardislu/uBlock-Origin-search-filter' >>'search-filter.txt'
 echo '! Licence: https://github.com/ardislu/uBlock-Origin-search-filter/blob/main/LICENSE' >>'search-filter.txt'
 
-# Append all lines from nuisances.txt which do not start with ! or whitespace
-grep -Ev '^(!|\s)' ./nuisances.txt >>'search-filter.txt'
+# Append all lines from nuisances.txt which do not start with ! or whitespace or is an empty line
+grep -Ev '^(!|\s)|^$' ./nuisances.txt >>'search-filter.txt'
 
 # Create search engine filters for all websites on websites.txt
-grep -Ev '^(!|\s)' ./websites.txt | tr -d '\r' | while read -r line; do
+grep -Ev '^(!|\s)|^$' ./websites.txt | tr -d '\r' | while read -r line; do
   echo "google.*##.g:has(a[href*=\"$line\"])" >>'search-filter.txt'    # Google
   echo "bing.*##.b_algo:has(a[href*=\"$line\"])" >>'search-filter.txt' # Bing
   echo "yahoo.*##li:has(a[href*=\"$line\"])" >>'search-filter.txt'     # Yahoo
